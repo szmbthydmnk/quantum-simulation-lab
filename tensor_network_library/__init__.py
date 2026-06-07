b969235d15c3a2dcc89ab7c0a5e2913bc8b9814d
Dominik Szombathy <szmbthy.dmnk@gmail.com>
2026-06-07 11:16:59 +0200

    Update __init__.py

--- tensor_network_library/__init__.py
+++ tensor_network_library/__init__.py
@@ -4,14 +4,3 @@
 TensorNetworkLibrary: A Python library for building and manipulating tensor networks, with a focus on 1D quantum lattice systems. Provides core data structures, common Hamiltonian MPOs, state preparation helpers, and algorithms like DMRG and TEBD.
 """
 
-from .core import *
-from .hamiltonian import *
-from .states import *
-from .algorithms import *
-
-__all__ = [
-    *core.__all__,
-    *hamiltonian.__all__,
-    *states.__all__,
-    *algorithms.__all__
-]
